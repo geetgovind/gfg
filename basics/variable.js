47f9ae3fb8512013d67c4335bb9db09481e0a26d
geetgovind <geetg.2902@gmail.com>
2024-01-04 14:35:40 +0530

    initial commit

---
+++ basics/variable.js
@@ -0,0 +1,25 @@
+/*
+* Why Variables?
+* What is variables?
+* Different ways to create variables.
+*/
+
+var message; // declaring variable
+message = "Hello Geeks"; // Assigning value to variable
+message = "JavaScript is needed for web development";
+console.log(message);
+
+// var is re-writtable
+
+let text;
+text = "javaScript is best";
+text = "JavaScript is new language having many libraries";
+console.log(text);
+
+// let is also re-writtable
+
+const myname = "Geet Govind"; // used for constatnt values
+// declaration must be initialised on first instance
+//myname = "geet"; 
+// not re-writable
+console.group(myname);
