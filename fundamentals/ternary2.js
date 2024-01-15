fbad4e15b809aa78d3f1ef0f32687c1958509c61
geetgovind <geetg.2902@gmail.com>
2024-01-15 14:32:56 +0530

    Completed Nested  Ternary Operators

--- fundamentals/ternary2.js
+++ fundamentals/ternary2.js
@@ -1,19 +1,8 @@
-console.log(`Ternary Operators - 1`);
+console.log(`Ternary Operators - 2, Nested Ternary Operator`);
 
 const readlineSync = require("readline-sync");
 
 let marks = readlineSync.question("Enter your marks in subject : ");
-const passingMarks = 33;
-const result = marks >= passingMarks ? true : false;
-
-if (result) {
-  console.log(
-    `As ${marks} is greter than or equals to the passing marks. You have cleared the subject. `
-  );
-} else {
-  console.log(`As ${marks} is less than passing marks. You need to work hard.`);
-}
-
 marks = Number(marks);
 let gradePoint;
 if (marks >= 0 && marks <= 100) {
