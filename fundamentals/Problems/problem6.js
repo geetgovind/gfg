833d5d9b28a388b840bf1e4d61bb228eef51f5e1
geetgovind <geetg.2902@gmail.com>
2024-01-16 10:34:31 +0530

    Fizz Buzz Problem Solved

---
+++ fundamentals/Problems/problem6.js
@@ -0,0 +1,17 @@
+const title = `You are given a number  and you have to print your answer according to the following:
+If the number is divisible by 3, you print "Fizz" (without quotes)
+If the number is divisible by 5, you print "Buzz" (without quotes)
+If the number is divisible by both 3 and 5, you print "FizzBuzz" (without quotes)
+In any other case, you print the number itself`;
+
+const number = 57676;
+
+if(number % 3 === 0 && number % 5 === 0){
+    console.log("FizzBuzz")
+}else if(number % 5 === 0){
+    console.log("Buzz")
+}else if(number % 3 === 0 ){
+    console.log("Fizz")
+}else{
+    console.log(number)
+}
