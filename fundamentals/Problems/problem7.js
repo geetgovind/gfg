ba3b041b7946f2faec6efee5cef68817bff10743
geetgovind <geetg.2902@gmail.com>
2024-01-16 17:27:58 +0530

    Nullish Coalescing txt

---
+++ fundamentals/Problems/problem7.js
@@ -0,0 +1,11 @@
+const title = `Given a number, represents number of apples in a bag. You and your friend are playing a game. Both of you are picking one apple turn wise from the bag. It is given that first attempt is always by you. The person picking the last apple will be the winner. Find out the winner if the quantity of apples is given to you as a number.
+If you will win: Print "You" (without quotes)
+If your friend will win: Print "Friend" (without quotes)`;
+
+const number = 57;
+
+if(number % 2===0){
+    console.log("Friend")
+}else{
+    console.log("You")
+}
