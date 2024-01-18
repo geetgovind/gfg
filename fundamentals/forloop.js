f0abcb908fe402400a7e65ca516e3952dce46fd2
geetgovind <geetg.2902@gmail.com>
2024-01-18 17:35:54 +0530

    Completed For loop

---
+++ fundamentals/forloop.js
@@ -0,0 +1,12 @@
+const title = `FOR Loop`;
+console.log(title);
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+console.log("loop execution done...");
+
+const fullName = "Geet Govindn";
+for(let i = 0; i<fullName.length; i++){
+    console.log(fullName[i]);
+}
