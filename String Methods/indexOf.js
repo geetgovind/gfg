997efc520ebac6905af6a15ad4eada7e8c049da2
geetgovind <geetg.2902@gmail.com>
2024-01-24 14:52:48 +0530

    indexOf methos of Strings

---
+++ String Methods/indexOf.js
@@ -0,0 +1,10 @@
+const title = `String Method - indexOf`;
+console.log(title);
+console.log("------------------------------------------");
+const str = "geeks ofr geeks"
+console.log(str.indexOf('r'));
+console.log("------------------------------------------");
+console.log(str.indexOf('g', 3));
+console.log("------------------------------------------");
+
+console.log("------------------------------------------");
