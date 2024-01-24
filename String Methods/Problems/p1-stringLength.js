8fa69479d89e3ecd2f9591cf8864804a48079f47
geetgovind <geetg.2902@gmail.com>
2024-01-24 09:24:44 +0530

    Started problems and solved first Sting length

---
+++ String Methods/Problems/p1-stringLength.js
@@ -0,0 +1,9 @@
+const title = `You are given a string s. You need to find the length of the string.`;
+console.log(title);
+console.log("--------------------------------------");
+
+function strLength(str){
+    return str.length;
+}
+const result = strLength("Geet Govind");
+console.log(result);
