af9745b42e784acc9cc8ae427732a24ee0414d3f
geetgovind <geetg.2902@gmail.com>
2024-01-24 17:16:39 +0530

    Completed String Cases Upper and Lower

---
+++ String Methods/stringCases.js
@@ -0,0 +1,7 @@
+const title = `JS String Method - toUpperCase() and toLowerCase()`;
+console.log(title);
+console.log("------------------------------");
+
+const str = "javaScript from Geeksforgeeks";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
