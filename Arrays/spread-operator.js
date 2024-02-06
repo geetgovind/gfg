d8bb2c25a19d179f6b2531dd1059e1fdb53c479b
geetgovind <geetg.2902@gmail.com>
2024-02-06 17:26:37 +0530

    Completed Spread Operator

--- Arrays/spread-operator.js
+++ Arrays/spread-operator.js
@@ -2,3 +2,13 @@ const title = `Spread Operator`;
 console.log(title);
 console.log("------------------------");
 
+const arr = [1,2,3,4,5,6];
+console.log("Spreaded Array -", ...arr);// spreading the objects in the array
+
+console.log("Array - ", arr)
+
+const arr2 = [7,8,9,10];
+const arr3 = [...arr, ...arr2];
+console.log("Merged array with spread array method- ", arr3)
+const arr1 =[...arr, 2,4,6,...arr2, 7,8,];
+console.log(`Added elemets in array - `, arr1)
