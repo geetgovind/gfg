ef2fa4d26e29fccd065253ecb10373cb571b5248
geetgovind <geetg.2902@gmail.com>
2024-02-07 14:40:37 +0530

    Objects and their Properties

--- Objects/object_properties.js
+++ Objects/object_properties.js
@@ -1,9 +1,10 @@
 const title = `Objects in JS and it's properties`;
 console.log(title);
 console.log("-------------------------------");
-
+// objects are most complex data structures in JS
+// Must learn this for using APIs 
 let schoolName = {
-    name: "Kendriya Vidyalaya",
+    name: "Kendriya Vidyalaya", // property
     location: "New Delhi",
     established: 1965,
     displayInfo: function () {
@@ -12,3 +13,36 @@ let schoolName = {
 };
 
 schoolName.displayInfo();
+
+const obj = {}// object literal method
+console.log(typeof obj); // object
+// key must be a string, value can be any data type
+const details = {
+    name : "Geet Govind",
+    job : "Web Developer",
+    age : 25,
+    1 : "Number One"   // key 1 is going to be converted in string implicitily
+}
+console.log(details);
+/**
+ * {
+  '1': 'Number One', // key 1 is converted in a string
+  name: 'Geet Govind',
+  job: 'Web Developer',
+  age: 25
+}
+ */
+
+const profile = {
+    name : "Geet Govind",
+    course : ["html", "CSS", "JS", "reactjs", "vitejs", "redux", "nextjs"],
+    "is Admin" : true 
+}
+console.log(profile)
+console.log(profile.name)
+console.log(profile.course)
+console.log(profile["course"]) // pass as the string only
+// why we need both methods
+// because of the key naming in objects can hace spaces too & it can be called using .- method
+
+console.log(profile["is Admin"]);
